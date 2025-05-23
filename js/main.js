9421819bb91bb8f082ff08eeb724cccc72d4a709
Anup Jonchhe <ajonchhe@prologuemedicines.com>
2025-05-23 12:14:38 -0400

    adding Ear of Anup to this page

--- js/main.js
+++ js/main.js
@@ -1,38 +1,67 @@
 
+
+function isCardComingSoon(card) {
+    label = card.getElementsByClassName("card-label")[0].innerText.toLowerCase().replace("\n", " ")
+    return COMING_SOON_PAGES.includes(label)
+}
+
+
+
+CARDS = [
+    { label: "myself", link: "about-me", isComplete: true },
+    { label: "software engineering", link: "software-engineering", isComplete: false },
+    { label: "music", link: "music", isComplete: false },
+    { label: "writing", link: "writing", isComplete: false },
+    { label: "projects", link: "projects", isComplete: false },
+]
+
+COMING_SOON_PAGES = [
+    "software engineering",
+    "music",
+    "writing",
+]
 //Coming soon for cards
 const cards = document.getElementsByClassName("item-card")
 for (const card of cards) {
     card.onmouseover = () => {
         //get card-label div inside card
         card.dataset.defaultText = card.getElementsByClassName("card-label")[0].innerText
-        card.getElementsByClassName("card-label")[0].innerText = "Coming Soon"
+        label = card.dataset.defaultText.toLowerCase().replace("\n", " ")
+        if (COMING_SOON_PAGES.includes(label)) {
+            card.getElementsByClassName("card-label")[0].innerText = "Coming Soon"
+        } else {
+            // apply card-hover class  
+            console.log("hovering")
+            card.classList.add("card-hover");
+        };
     }
     card.onmouseout = () => {
         //get card-label div inside card
         card.getElementsByClassName("card-label")[0].innerText = card.dataset.defaultText
+        card.classList.remove("card-hover");
     }
 }
 
+const track_page = document.getElementById("content-track-page")
 const track = document.getElementById("content-track")
-window.onmousedown = e => {
+
+track_page.onmousedown = e => {
     console.log(e.pageX)
     track.dataset.mouseDownAt = `${e.clientX}`;
-    console.log("Mouse Down at:", track.dataset.mouseDownAt);
 }
 
-window.onmouseup = e => {
+track_page.onmouseup = e => {
     track.dataset.mouseDownAt = "0";
     track.dataset.prevPercentage = track.dataset.percentage;
-    console.log("Mouse up at:", e.clientX);
 }
 
-window.onmousemove = e => {
+track_page.onmousemove = e => {
     if (track.dataset.mouseDownAt === "0") return; //mouse is up
 
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-          maxDelta = window.innerWidth / 2;
+        maxDelta = window.innerWidth / 2;
 
-    const movementPercentage = (mouseDelta/maxDelta) * -100;
+    const movementPercentage = (mouseDelta / maxDelta) * -100;
 
     let trackPercentage = Math.max(Math.min(
         parseFloat(track.dataset.prevPercentage) + movementPercentage,
@@ -41,8 +70,43 @@ window.onmousemove = e => {
     // track.style.transform = `translate(-${trackPercentage}%, -50%)`
     track.animate({
         transform: `translate(${trackPercentage}%, -50%)`
-    }, {duration: 1200, fill: "forwards"});
+    }, { duration: 1200, fill: "forwards" });
 
     //update percentage to communicate to onmouseup (is there not an on mouse drag???)
     track.dataset.percentage = `${trackPercentage}`;
 }
+
+const cardObjects = document.querySelectorAll(".item-card");
+console.log(cardObjects)
+
+cardObjects.forEach(card => {
+    const card_data = CARDS.find(c => c.label === card.innerText.toLowerCase().replace("\n", " "))
+    console.log(card_data)
+    console.log(card_data.isComplete)
+    if (card_data.isComplete) {
+        console.log(`card: ${card_data.label} is complete`)
+        card.onclick = () => {
+            console.log(card_data)
+            const card_section_id = card_data.link;
+            const cardSection = document.getElementById(card_section_id);
+            console.log("Completed Card is Clicked") 
+            if (cardSection) {
+                cardSection.style.display = "block";
+                cardSection.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }
+})
+
+// if (myselfCard && myselfCard.innerText === "Myself") {
+//     myselfCard.parentElement.onclick = () => {
+//         const aboutMeSection = document.getElementById("about-me");
+//         if (aboutMeSection) {
+//             aboutMeSection.style.display = "block";
+//             aboutMeSection.scrollIntoView({ behavior: "smooth" });
+            
+
+
+//         }
+//     };
+// }
